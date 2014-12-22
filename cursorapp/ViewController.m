c6bdd163096ae6d44f856e3701a300f9ea94abae
jehna <jesse.luoto@gmail.com>
2014-12-22 15:28:31 +0200

    Fixed: Initial load first char press crash bug

--- cursorapp/ViewController.m
+++ cursorapp/ViewController.m
@@ -205,14 +205,15 @@ CGFloat insertScale = 1.0f;
 }
 
 - (void)load {
+    NSUserDefaults *ud = [NSUserDefaults standardUserDefaults];
+    if([ud objectForKey:@"currentTexts"] == nil) return;
+    
     for(UIView *view in self.textViews) {
         [view removeFromSuperview];
     }
     self.textViews = [NSMutableArray array];
     
     currentChar = 0;
-    NSUserDefaults *ud = [NSUserDefaults standardUserDefaults];
-    if([ud objectForKey:@"currentTexts"] == nil) return;
     self.currentTexts = [(NSArray *)[ud objectForKey:@"currentTexts"] mutableCopy];
     for(int i = 0; i < self.currentTexts.count; i++) {
         [self.currentTexts setObject:[(NSString *)[self.currentTexts objectAtIndex:i] mutableCopy] atIndexedSubscript:i];
